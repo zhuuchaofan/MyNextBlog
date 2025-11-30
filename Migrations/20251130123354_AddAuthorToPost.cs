810c7727c0ac2a44f9fc0805374358598f125ff2
朱超凡 <chaofanzhu@vip.qq.com>
2025-11-30 22:12:33 +0900

    数据库更新

---
+++ Migrations/20251130123354_AddAuthorToPost.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyTechBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthorToPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Posts",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Users_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Users_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Posts");
+        }
+    }
+}
