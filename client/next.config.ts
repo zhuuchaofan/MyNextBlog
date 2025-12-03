c76578d2cf54265fd3924f0847e5f66b52db4671
朱超凡 <chaofanzhu@vip.qq.com>
2025-12-03 23:11:09 +0900

    docker部署（不知道能不能成

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // 开启 Docker 优化模式
   async rewrites() {
     return [
       {
-        source: '/api/backend/:path*', // 前端请求的假地址
-        destination: 'http://localhost:5095/api/:path*', // 后端真实地址
+        source: '/api/backend/:path*',
+        // 优先读取环境变量 (Docker 内部通信)，本地开发默认用 localhost:5095
+        destination: `${process.env.BACKEND_URL || 'http://localhost:5095'}/api/:path*`,
       },
     ];
   },
